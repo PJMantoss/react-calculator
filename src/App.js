0cb16079ae7c777b50de471822fcf6879351e772
pjmantoss <joelptoss@gmail.com>
2020-04-17 19:11:23 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 // variables
-const operator = /[x/+-]/, endsWithOperator = /[x+-/]$/, endsWithNegativeSign = /[x/+]-$/,
+const operator = /[x/+-]/, 
+    endsWithOperator = /[x+-/]$/, 
+    endsWithNegativeSign = /[x/+]-$/,
+    clearStyle = { background: "#ac3939" },
+    equalStyle = {
+      background: "#004466",
+      position: "absolute",
+      height: 130,
+      bottom: 5
+    },
 
 export class App extends Component {
   render() {

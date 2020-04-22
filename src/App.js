8288175fdd9eb86d3be67d4a92b509ab6b5a4d14
pjmantoss <joelptoss@gmail.com>
2020-04-22 17:49:46 +0100

    update App.js and KeyPads

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import '/App.css';
 const operator = /[x/+-]/, 
     endsWithOperator = /[x+-/]$/, 
     endsWithNegativeSign = /[x/+]-$/,
-    clearStyle = { background: "#ac3939" },
     equalStyle = {
       background: "#004466",
       position: "absolute",

88356f4a9ae1bd833cb94900767d21c6cbb4c258
pjmantoss <joelptoss@gmail.com>
2020-04-22 16:27:15 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -40,6 +40,15 @@ export class App extends Component {
       while(endsWithOperator.test(expression)){
         expression = expression.slice(0, -1);
       }
+
+      expression = expression.replace(/x/g, "*").replace(/-/g, "-");
+      let answer = Math.round(1000000000000 * eval(expression)) / 1000000000000;
+      this.setState({
+        currentVal: answer.toString(),
+        formula: expression.replace(/\*/g, ".").replace(/-/g, "-") + "=" + answer,
+        prevVal: answer,
+        evaluated: true
+      });
     }
   }
 

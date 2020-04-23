64a24ede9d1e7c6e2470479cd993748f8bd4c9f2
pjmantoss <joelptoss@gmail.com>
2020-04-23 15:46:43 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -157,6 +157,11 @@ class App extends Component {
               operators={this.handleOperators} 
           />
         </div>
+        <div className="love">
+            <span>Made With</span>
+            <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
+            <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
+        </div>
       </div>
     )
   }

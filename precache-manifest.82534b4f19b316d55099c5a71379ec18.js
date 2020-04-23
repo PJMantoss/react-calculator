428ff4f1aae319c063e6f5ec73887cde7b42483e
pjmantoss <joelptoss@gmail.com>
2020-04-23 18:37:36 +0100

    Updates

---
+++ precache-manifest.82534b4f19b316d55099c5a71379ec18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab29d804acca332424253645a395f8f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "351d76b7ed54ce42f908",
+    "url": "/react-calculator/static/css/main.fb0f0d37.chunk.css"
+  },
+  {
+    "revision": "19bc0ffb8806272aca23",
+    "url": "/react-calculator/static/js/2.e9489090.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.e9489090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351d76b7ed54ce42f908",
+    "url": "/react-calculator/static/js/main.e678cae3.chunk.js"
+  },
+  {
+    "revision": "1efbf9ef7b1c2f1ed578",
+    "url": "/react-calculator/static/js/runtime-main.12eaf142.js"
+  }
+]);
